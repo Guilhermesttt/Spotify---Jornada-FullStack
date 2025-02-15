3cc60baac569541832f66447de20c5da98636ecf
Guilherme Sant'ana <you@example.com>
2025-02-15 03:37:25 -0300

    ajuste de process novamente

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,10 +1,9 @@
 //  Fetch ou Axios
-import "dotenv/config";
 import axios from "axios";
 
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3000/api";
-const URL = "https://spotify-jornada-fullstack.onrender.com/api/";
+const URL = "https://spotify-jornada-fullstack.onrender.com/api";
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
